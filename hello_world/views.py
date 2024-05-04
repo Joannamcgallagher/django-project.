a9fcb17b26394b4e248172a35e7996a4521b89e3
Joannamcgallagher <joannamcgallagher@gmail.com>
2024-05-04 14:38:28 +0000

    Add if//else for request.method on Hello page

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTED something")
+    else:
+        return HttpResponse(request.method)
 
+''' 
+Adjust the view so that if the request method is POST, a HttpResponse is returned, that says, "You must have POSTed something".
+Otherwise, return an HttpResponse so that, instead of plain text, we return the method of the request object.'''
